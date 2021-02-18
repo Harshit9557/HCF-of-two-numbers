b13fec42a370e8542dd5c670492d88b67ac3ab8d
Harshit9557 <57794543+Harshit9557@users.noreply.github.com>
2021-02-18 16:04:36 +0530

    Add files via upload

---
+++ hcf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,h=0,l,i;
+    printf("Enter values of n1,n2 : \n");
+    scanf("%d\n%d",&n1,&n2);
+    for(i=1;i<=n1,i<=n2;i++)
+        if(n1%i==0 && n2%i==0)
+            h=i;
+    printf("HCF : %d",h);
+    return 0;
+}
